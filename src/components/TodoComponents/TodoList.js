8f4525a663550364756e0e50e4936e60f0aaaeab
Jayne Carmichael Norrie <39460834+jaynecn@users.noreply.github.com>
2019-10-07 20:10:03 +0100

    using a ternary to change classnames in TodoList.js

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import './Todo.css';
 
 function TodoList(props) {
+
+  // getStyle = () => {
+  //   return {
+  //     background: '#f4f4f4',
+  //     padding: '10px',
+  //     borderBottom: '1px #ccc dotted',
+  //     textDecoration: this.props.todo.completed ? 'line-through' : 'none'
+  //   }
+  // }
+
   // console.log(props);
   return (
     <div>
@@ -9,14 +20,34 @@ function TodoList(props) {
         props.taskList.map((info) => {
           // console.log(info);
           return (
-          <div className="list-of-tasks"
-            key={info.id}
-            >{info.task}
-            <button className="clicked-button"
-              value={info.task}
-              onClick={props.markCompleted}
-              >x</button>
-          </div>)
+          <div>
+             {
+            info.completed === true 
+              ? <div className="checked-tasks" key=     {info.id}>
+                  <div className="mini-div">
+                    <input type="checkbox"/>
+                    {info.task}           
+                  </div>
+                  <button className="clicked-button"
+                    value={info.task}
+                    onClick={props.markCompleted}
+                    >X
+                    </button>
+               </div>
+
+              : <div className="list-of-tasks" key=     {info.id}>
+                  <div className="mini-div">
+                    <input type="checkbox"/>
+                    {info.task}           
+                  </div>
+                  <button className="clicked-button"
+                    value={info.task}
+                    onClick={props.markCompleted}
+                    >X
+                  </button>
+                </div>
+            }
+          </div>);
         })
       }
     </div>
